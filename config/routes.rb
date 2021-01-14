381f4b8757701307253918240f4efc9af1059bf6
DavidRMorphew <morphewd@umich.edu>
2021-01-13 19:58:48 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'genres/edit'
+
+  get 'genres/update'
+
+  get 'genres/resources'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, :songs, :genres
 end
